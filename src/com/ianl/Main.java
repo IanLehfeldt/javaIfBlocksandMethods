7f9772ce4e2fd76d6328819db1ef0a400f3fa506
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-05-31 12:50:35 -0500

    created two methods that take in parameters and can display based off of returned values

--- src/com/ianl/Main.java
+++ src/com/ianl/Main.java
@@ -22,7 +22,8 @@ public class Main {
 		score = 10000;
 		levelCompleted = 8;
 		bonus = 200;
-		calculateScore(gameOver, score, levelCompleted, bonus);
+
+		highScore = calculateScore(gameOver, score, levelCompleted, bonus);
 		System.out.println("Your final score was " + highScore);
 
 		//You can't access variables created in a code block outside of the block
@@ -37,7 +38,10 @@ public class Main {
 //			int finalScore = score + (levelCompleted * bonus);
 //			System.out.println("Your final score was " + finalScore);
 //		}
-//
+		displayHighScorePosition("Sam", calculateHighScorePosition(1500));
+		displayHighScorePosition("Taylor", calculateHighScorePosition(900));
+		displayHighScorePosition("Jane", calculateHighScorePosition(400));
+		displayHighScorePosition("Janet", calculateHighScorePosition(50));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -49,5 +53,22 @@ public class Main {
 		//will only be reached if the previous block's return wasn't hit
 		//we return -1 because it represents an error or an invalid value/value not found
 		return -1;
+	}
+
+	//void functions/methods are called procedures
+	public static void displayHighScorePosition(String playerName, int highScorePosition) {
+		System.out.println(playerName+" managed to get into position "+highScorePosition+" on the highscore table.");
+	}
+
+	public static int calculateHighScorePosition(int playerHighScore) {
+		if(playerHighScore > 1000) {
+			return 1;
+		} else if (playerHighScore > 500 && playerHighScore < 1000) {
+			return 2;
+		} else if (playerHighScore > 100 && playerHighScore < 500) {
+			return 3;
+		} else {
+			return 4;
+		}
 	}
 }
